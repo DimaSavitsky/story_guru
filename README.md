35dc70b8a23b23042f538b1a8084067d68d7df2e
Dmitry Savitski <dimasavitski@gmail.com>
2016-10-07 16:27:42 +0300

    Add Devise auth and registration

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # README
+
+## To generate '.slim' views out of '.erb' ones:
+erb2slim -d DIR
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable, :recoverable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,  :rememberable
+
+end
